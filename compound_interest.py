a6ab801e2ef048ed641c4c4f64a58ed3f78a86d0
barbalu <47788619+barbalu@users.noreply.github.com>
2024-04-27 00:45:21 +0630

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -22,3 +22,6 @@ if __name__ == "__main__":
     r = float(input("Enter the rate of interest: "))
 
     print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
+
+Switched to a new branch 'bug-fix-typo'
+bug-fix-typo
