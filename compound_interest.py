95670773b99ec2678aab28f4a385dca8bd05d0c2
gabedenmark <131288941+gabedenmark@users.noreply.github.com>
2024-05-21 16:47:24 -0400

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -3,13 +3,13 @@
 
 # Author: Upkar Lidder (IBM)
 
-# Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
+Input:
+p, principal amount
+t, time period in years
+r, annual rate of interest
 
-# Output:
-# compound interest = p * (1 + r/100)^t
+Output:
+compound interest = p * (1 + r/100)^t
 
 
 def compound_interest(p, t, r):
